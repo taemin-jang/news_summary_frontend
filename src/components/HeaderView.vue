1d8ace981c8ea3abb5c402af3b68bd067ff9793f
taemin-Jang <jtm990506@gmail.com>
2023-03-21 22:24:23 +0900

    (#1) chore : Vuetify3 + vite

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="logo">
+      <p>News Summary</p>
+    </div>
+    <div class="search">
+      <input type="text" />
+    </div>
+    <div class="dark-mode">다크모드</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
