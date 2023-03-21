bc6d5d25beab8cb24205d35f920d47803c94bfce
taemin-Jang <jtm990506@gmail.com>
2023-03-21 23:38:59 +0900

    (#1) chore : material-design-icons-iconfont

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,21 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+import { aliases, md } from "vuetify/iconsets/md";
+import { mdi } from "vuetify/iconsets/mdi";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides,
+  icons: {
+    defaultSet: "md",
+    aliases,
+    sets: {
+      md,
+      mdi,
+    },
+  },
+});
