a8900c65d1c0e730db390365b26635f18619f67b
taemin-Jang <jtm990506@gmail.com>
2023-03-22 14:11:29 +0900

    (#3) feat : sidebar 추가

---
+++ src/components/common/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-navigation-drawer>
+    <v-list>
+      <v-list-item title="abc" value="abc"></v-list-item>
+      <v-list-item title="abc" value="abc"></v-list-item>
+      <v-list-item title="abc" value="abc"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SideBar",
+});
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
   <Header></Header>
+  <SideBar></SideBar>
+  <v-main>
+    <router-view />
+  </v-main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 // Components
-import Header from "@/components/HeaderView.vue";
+import Header from "@/components/common/HeaderView.vue";
+import SideBar from "@/components/common/SideBar.vue";
 export default defineComponent({
   name: "HomeView",
 
   components: {
     Header,
+    SideBar,
   },
 });
 </script>
