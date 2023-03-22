4a294f21d5912a3939a42fe0b8ac014782b17a86
taemin-Jang <jtm990506@gmail.com>
2023-03-22 23:20:58 +0900

    (#1) chore : sass sass-loader 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
-
-const path = require('path')
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,12 +10,13 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
+      // styles: { configFile: "src/assets/scss/settings.scss" },
     }),
   ],
-  define: { 'process.env': {} },
+  define: { "process.env": {} },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
@@ -32,4 +32,4 @@ export default defineConfig({
     ]
   },
   */
-})
+});
