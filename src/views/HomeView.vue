0d2fe2effde383c5563e889aac12a91d030ab403
taemin-Jang <jtm990506@gmail.com>
2023-03-25 01:29:05 +0900

    (#4) add : common component 연결

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
-  <Header></Header>
-  <SideBar></SideBar>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-card>
+    <Header></Header>
+    <SideBar></SideBar>
+    <MainSheet> </MainSheet>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -12,12 +12,14 @@ import { defineComponent } from "vue";
 // Components
 import Header from "@/components/common/HeaderView.vue";
 import SideBar from "@/components/common/SideBar.vue";
+import MainSheet from "@/components/common/MainSheet.vue";
 export default defineComponent({
   name: "HomeView",
 
   components: {
     Header,
     SideBar,
+    MainSheet,
   },
 });
 </script>
