0fdf5d0359b8a4f2d2e0f8594108df37aa46b7b1
taemin-Jang <jtm990506@gmail.com>
2023-03-25 01:32:20 +0900

    (#7) add : /politics

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
+/** @format */
+
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: () => import("@/views/HomeView.vue"),
+    children: [
+      {
+        path: "politics",
+        component: () => import("@/components/pages/PoliticsView.vue"),
+      },
+    ],
   },
   // {
   //   path: '/about',
