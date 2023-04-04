ba899ff668e36b70073b9ef3940361afa4923ca2
taemin-Jang <jtm990506@gmail.com>
2023-04-04 15:49:09 +0900

    (#1) chore : axios 설치 및 전역 변수 설정

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import axios from "axios";
 
-loadFonts()
+loadFonts();
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App);
+
+// axios 전역 변수 설정
+app.config.globalProperties.$axios = axios;
+
+app.use(router).use(vuetify).mount("#app");
