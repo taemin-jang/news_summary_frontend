f5c68ba6908a12700196efc5974e796fc7202652
taemin-Jang <jtm990506@gmail.com>
2023-05-07 23:17:43 +0900

    naver api 연동

--- src/components/pages/PoliticsView.vue
+++ src/components/pages/PoliticsView.vue
@@ -1,5 +1,3 @@
-<!-- @format -->
-
 <template>
   <v-card color="innerBg" :elevation="4" width="70%">
     <v-card-title>경제</v-card-title>
@@ -41,29 +39,50 @@
         </v-sheet>
       </v-col>
       <v-col>
-        <v-sheet :elevation="2" color="newsBg" class="pa-2 ma-2 rounded-t-xl">
+        <v-sheet
+          :elevation="2"
+          color="newsBg"
+          class="pa-2 ma-2 rounded-t-xl"
+          v-if="arr">
           <v-img
             class="bg-white rounded-xl elevation-4"
             width="100%"
-            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+            :src="arr[0]?.images[0]"
             cover></v-img>
-          <div class="text-subtitle-1 font-weight-bold mt-1">경제 제목</div>
-          <p class="text-start">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
+          <div class="text-subtitle-1 font-weight-bold mt-1">
+            {{ arr[0]?.title }}
+          </div>
+          <p class="text-start" v-html="arr[0]?.description"></p>
         </v-sheet>
       </v-col>
     </v-row>
   </v-card>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import AxiosService from "@/services/AxiosService";
+import { NaverSearch, NaverResponse } from "@/types/NaverSearch";
+import { reactive, ref } from "vue";
 
-export default defineComponent({});
+let AxiosInstance = AxiosService;
+// const naverSearch: NaverSearch = reactive({
+//   title: ref(""),
+//   description: ref(""),
+//   link: ref(""),
+//   originallink: ref(""),
+//   pubDate: ref(""),
+//   images: ref([]),
+// });
+let arr: NaverResponse[] = [];
+async function searchNaver() {
+  arr = await AxiosInstance.getNaver("코스피");
+  console.log(arr);
+}
+
+searchNaver();
+console.log(arr);
+// console.log(data);
+// console.log(data.description);
 </script>
 
 <style scoped></style>
