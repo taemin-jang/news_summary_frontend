514093d3f731b129528885dd8a4570cb9f6b1b5a
taemin-Jang <jtm990506@gmail.com>
2023-05-10 17:51:16 +0900

    kakao OAuth 환경 설정

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import axios from "axios";
-
+declare const Kakao: any;
 loadFonts();
 
 const app = createApp(App);
 
-// axios 전역 변수 설정
-app.config.globalProperties.$axios = axios;
-
 app.use(router).use(vuetify).mount("#app");
+
+// Kakao Developers에서 요약 정보 -> JavaScript 키
+if (Kakao) {
+  Kakao.init("830f4c1628e35b3abb879f8f98fd7841");
+}
