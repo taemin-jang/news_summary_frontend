02e7fb8676ba64a75abd04ca826d09d31f389410
taemin-Jang <jtm990506@gmail.com>
2023-05-18 00:59:29 +0900

    kakao oauth login 구현

--- src/components/common/HeaderView.vue
+++ src/components/common/HeaderView.vue
@@ -8,7 +8,6 @@
         append-inner-icon="mdi:mdi-magnify"
         single-line
         hide-details></v-text-field>
-      <!-- @click:append-inner="onClick" -->
     </v-card-text>
     <v-spacer></v-spacer>
     <v-btn
@@ -16,28 +15,70 @@
       @click="toggleTheme"
       v-if="theme.current.value.dark"></v-btn>
     <v-btn icon="light_mode" @click="toggleTheme" v-else></v-btn>
+    <div v-if="!profile">
+      <v-btn
+        icon="mdi:mdi-account-outline"
+        @click="login"
+        v-if="theme.current.value.dark"></v-btn>
+      <v-btn icon="mdi:mdi-account" @click="login" v-else></v-btn>
+    </div>
+    <div v-else>
+      <v-btn>
+        <img
+          :src="profile.thumbnail_image"
+          alt="kakao_profile"
+          width="30"
+          height="30"
+          @click="mypage"
+          v-if="profile" />
+      </v-btn>
+    </div>
   </v-app-bar>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, Ref, onMounted } from "vue";
 import { useTheme } from "vuetify";
+import { useRouter } from "vue-router";
+import { useUserStore } from "@/store/user";
+import { UserProfile } from "@/types/KakaoLogin";
+import AxiosService from "@/services/AxiosService";
+
+const store = useUserStore();
 
 export default defineComponent({
   name: "HeaderView",
   setup() {
     const theme = useTheme();
-    console.log(theme);
+    const router = useRouter();
+    let profile: Ref<UserProfile | null> = ref(store.getUser);
+
+    onMounted(async () => {
+      await AxiosService.getKakaoProfile();
+    });
     return {
       theme,
+      profile,
       toggleTheme: () => {
         theme.global.name.value = theme.global.current.value.dark
           ? "light"
           : "dark";
+      },
+      login: () => {
+        router.push("/login");
+      },
+      mypage: () => {
+        router.push("/mypage");
       },
     };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+</style>
