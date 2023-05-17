e801e6f91fdb1293c59160cb5b7061994f49b24c
taemin-Jang <jtm990506@gmail.com>
2023-05-17 23:56:34 +0900

    UserStore 생성

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import { createPinia } from "pinia";
 import { loadFonts } from "./plugins/webfontloader";
 declare const Kakao: any;
 loadFonts();
 
+const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router).use(vuetify).mount("#app");
+app.use(router).use(vuetify).use(pinia).mount("#app");
 
 // Kakao Developers에서 요약 정보 -> JavaScript 키
 if (Kakao) {
---
+++ src/store/user.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { ref, Ref, computed } from "vue";
+import { UserProfile } from "@/types/KakaoLogin";
+
+export const useUserStore = defineStore("user", () => {
+  const user: Ref<UserProfile | null> = ref(null);
+  /**
+   * user의 정보를 가져오는 getters
+   */
+  const getUser = computed(() => user);
+  /**
+   * user 정보를 저장하는 actions
+   * @param userDTO user 데이터가 담겨 있음
+   */
+  const setUser = (userDTO: UserProfile) => {
+    user.value = userDTO;
+  };
+  return { user, getUser, setUser };
+});
