66f1b36960eb2abbaeb115b0c88303c343735bdc
taemin-Jang <jtm990506@gmail.com>
2023-05-24 00:46:06 +0900

    첫 화면 진입 시 user 정보 유무

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App',
-
-  data () {
-    return {
-      //
-    }
-  },
-})
+<script setup lang="ts">
+import { useUserStore } from "@/store/user";
+import { useRouter } from "vue-router";
+const store = useUserStore();
+const router = useRouter();
+if (store.getUser.value === null) {
+  router.push("/login");
+}
 </script>
