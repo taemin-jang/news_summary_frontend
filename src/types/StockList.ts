c7f1a87be0e75a5d2d59ac9d01a3284d166ea431
taemin-Jang <jtm990506@gmail.com>
2023-05-26 02:12:29 +0900

    stock, portfolio type 설정

---
+++ src/types/StockList.ts
@@ -0,0 +1,13 @@
+export interface Stock {
+  itmsNm: string;
+  mrktCtg: string;
+  srtnCd: string;
+}
+
+export interface Portfolio {
+  itmsNm: string;
+  mrktCtg: string;
+  srtnCd: string;
+  clpr: string;
+  vs: string;
+}
