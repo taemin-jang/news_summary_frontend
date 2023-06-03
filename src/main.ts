83be82afc1d1c9c3636e1ab24b1f8c02163e476d
taemin-Jang <jtm990506@gmail.com>
2023-06-03 18:43:53 +0900

    Add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { createPinia } from "pinia";
 import { loadFonts } from "./plugins/webfontloader";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 declare const Kakao: any;
 loadFonts();
-
+library.add(faSpinner);
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router).use(vuetify).use(pinia).mount("#app");
+app
+  .use(router)
+  .use(vuetify)
+  .use(pinia)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
 
 // Kakao Developers에서 요약 정보 -> JavaScript 키
 if (Kakao) {
