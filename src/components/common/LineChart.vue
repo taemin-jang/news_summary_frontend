3423c04797854dc2ae1251f0e0d6e972639c74a6
taemin-Jang <jtm990506@gmail.com>
2023-06-08 04:36:23 +0900

    Chart style color modify

--- src/components/common/LineChart.vue
+++ src/components/common/LineChart.vue
@@ -6,9 +6,11 @@ import { computed, defineProps, ref } from "vue";
 import { LineChart, useLineChart } from "vue-chart-3";
 import { Chart, ChartData, ChartOptions, registerables } from "chart.js";
 import { ChartDate } from "@/types/StockList";
+import { useTheme } from "vuetify";
 
 Chart.register(...registerables);
 
+const theme = useTheme();
 const props = defineProps(["portfolio"]);
 const dataValues = ref(
   props.portfolio.list.toReversed().map((data: ChartDate) => data.clpr)
@@ -26,7 +28,10 @@ const testData = computed<ChartData<"line">>(() => ({
     {
       label: props.portfolio.itmsNm,
       data: dataValues.value,
-      backgroundColor: ["#77CEFF", "#0079AF", "#123E6B", "#97B0C4", "#A5C8ED"],
+      backgroundColor: [theme.current.value.colors.fontColor],
+      borderColor: theme.current.value.colors.innerBg,
+      fill: theme.current.value.colors.innerBg,
+      fontColor: theme.current.value.colors.fontColor,
     },
   ],
 }));
@@ -37,6 +42,32 @@ const testData = computed<ChartData<"line">>(() => ({
 const options = computed<ChartOptions<"line">>(() => ({
   responsive: true,
   maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: true,
+      labels: {
+        color: theme.current.value.colors.fontColor,
+      },
+    },
+  },
+  scales: {
+    x: {
+      border: {
+        color: theme.current.value.colors.fontColor,
+      },
+      ticks: {
+        color: theme.current.value.colors.fontColor,
+      },
+    },
+    y: {
+      border: {
+        color: theme.current.value.colors.fontColor,
+      },
+      ticks: {
+        color: theme.current.value.colors.fontColor,
+      },
+    },
+  },
 }));
 const { lineChartProps } = useLineChart({
   chartData: testData,
